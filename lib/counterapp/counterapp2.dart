252a70ef6e57d3da2eceb8d06a03230dce2efa81
muhammedzaafran <zaafranmuhammed@gmal.com>
2023-12-12 19:41:04 +0530

    Done.

--- lib/counterapp/counterapp2.dart
+++ lib/counterapp/counterapp2.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class CounterApp extends StatefulWidget {
-  const CounterApp({super.key});
+class MyCounterApp extends StatefulWidget {
+  const MyCounterApp({super.key});
 
   @override
-  State<CounterApp> createState() => _CounterAppState();
+  State<MyCounterApp> createState() => _CounterAppState();
 }
 
-class _CounterAppState extends State<CounterApp> {
+class _CounterAppState extends State<MyCounterApp> {
   int count = 0;
   void incCount() {
     setState(() {
@@ -24,15 +24,17 @@ class _CounterAppState extends State<CounterApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.yellow,
       appBar: AppBar(
-        title: const Text("Counter app"),
+        title: const Text("My Counter App"),
+        backgroundColor: Colors.black,
       ),
       body: Container(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
           Center(
               child: Text(
-            "Count : $count ",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            "Counter : $count ",
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 35, fontStyle:FontStyle.normal),
           )),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -41,15 +43,16 @@ class _CounterAppState extends State<CounterApp> {
                   onPressed: () {
                     incCount();
                   },
-                  child: const Text("Increment")),
+                  child: const Text("INCREMENT")),
               const SizedBox(
-                width: 20,
+                width: 10,
+                height: 30,
               ),
               ElevatedButton(
                   onPressed: () {
                     decCount();
                   },
-                  child: const Text("Decrement"))
+                  child: const Text("DECREMENT"))
             ],
           )
         ]),
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:module2_flutter/Button/example1.dart';
 import 'package:module2_flutter/Row/example1.dart';
 import 'package:module2_flutter/counterapp/counterapp.dart';
 
+import 'counterapp/counterapp2.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-    home: RowSample(),
+    home: MyCounterApp(),
     );
   }
 }
