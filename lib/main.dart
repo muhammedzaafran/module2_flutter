94619a89e1b9156091c95a871084ae8925cd75df
muhammedzaafran <zaafranmuhammed@gmal.com>
2023-12-12 19:12:20 +0530

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:module2_flutter/Button/eg1.dart';
-import 'package:module2_flutter/Row/eg1.dart';
+import 'package:module2_flutter/Button/example1.dart';
+import 'package:module2_flutter/Row/example1.dart';
 import 'package:module2_flutter/counterapp/counterapp.dart';
 
 void main() {
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-    home: CounterApp(),
+    home: RowSample(),
     );
   }
 }
