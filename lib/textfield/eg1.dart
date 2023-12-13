7908570fe2a8fbf62bfcc5fe9a92e01611528eca
muhammedzaafran <zaafranmuhammed@gmal.com>
2023-12-13 11:40:58 +0530

    Done.

---
+++ lib/textfield/eg1.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Textfield extends StatefulWidget {
+  const Textfield({super.key});
+
+  @override
+  State<Textfield> createState() => _TextfieldState();
+}
+
+class _TextfieldState extends State<Textfield> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
