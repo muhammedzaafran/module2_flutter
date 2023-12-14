145414f38d14272840d36143d58c7e34f32f8376
muhammedzaafran <zaafranmuhammed@gmal.com>
2023-12-14 10:40:57 +0530

    Done.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:module2_flutter/Button/example1.dart';
 import 'package:module2_flutter/Row/example1.dart';
 import 'package:module2_flutter/counterapp/counterapp.dart';
 import 'package:module2_flutter/padding/eg1.dart';
+import 'package:module2_flutter/textfield/example1.dart';
 
 import 'button/example1.dart';
 import 'calculator_app/calculator1.dart';
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-    home:PaddingSample(),
+    home:Textfield(),
     );
   }
 }
--- lib/textfield/example1.dart
+++ lib/textfield/example1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Textfield extends StatefulWidget {
   const Textfield({super.key});
 
@@ -9,6 +10,40 @@ class Textfield extends StatefulWidget {
 class _TextfieldState extends State<Textfield> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Text field sample"),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          const Text(
+            "Login Form",
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic),
+          ),
+          Form(
+            child: SizedBox(
+              width: 150,
+              child: TextFormField(
+                cursorColor: Colors.green,
+                keyboardType: TextInputType.datetime,
+                decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.all(30),
+                    prefixIcon: const Icon(Icons.person),
+                    label: const Text("Name"),
+                    hintText: "Enter name",
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8))),
+              ),
+            ),
+          )
+        ]),
+      ),
+    );
   }
 }
