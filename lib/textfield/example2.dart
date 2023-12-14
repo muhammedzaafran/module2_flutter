2f45a08405951b0964fa300cdc2f9dc7403dd536
muhammedzaafran <zaafranmuhammed@gmal.com>
2023-12-14 11:57:01 +0530

    Done.

--- lib/textfield/example2.dart
+++ lib/textfield/example2.dart
@@ -4,41 +4,49 @@ class TextfieldSample extends StatefulWidget {
   const TextfieldSample({super.key});
 
   @override
-  State<TextfieldSample> createState() => _TextfieldSampleState();
+  State<TextfieldSample> createState() => _TextfieldState();
 }
 
-class _TextfieldSampleState extends State<TextfieldSample> {
+class _TextfieldState extends State<TextfieldSample> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.teal,
       appBar: AppBar(
         backgroundColor: Colors.black,
         title: const Text("TEXT FIELD SAMPLE"),
-        centerTitle: true,
       ),
-
       body: Container(
         alignment: Alignment.center,
-        child: const Column(
-          children: [
-            Text("LOGIN HERE", style: TextStyle(
-              fontStyle: FontStyle.normal, fontWeight: FontWeight.bold, fontSize: 23,
-          height: 5,
-
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+          const Text(
+            "LOGIN HERE",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.normal),
+          ),
+          Form(
+            child: SizedBox(
+              width: 300,
+              child: TextFormField(
+                cursorColor: Colors.green,
+                keyboardType: TextInputType.name,
+                decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.all(30),
+                    prefixIcon: const Icon(Icons.person),
+                    label: const Text("username"),
+                    hintText: "username here",
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8))),
+              ),
             ),
-            ),
-             Form(
-                 child:SizedBox(
-                   width: 100,
-                 ) ),
-            TextField(
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(hintText: "username"),
-              style: TextStyle(fontSize: 14),keyboardType: TextInputType.name,
-
-            ),
-          ],
-        ),
+          )
+        ]),
       ),
     );
   }
