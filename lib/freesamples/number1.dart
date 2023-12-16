2cd9a32af014e7baf99ca4ef9fdf3bd456ac9509
muhammedzaafran <zaafranmuhammed@gmal.com>
2023-12-16 11:23:46 +0530

    Done.

--- lib/freesamples/number1.dart
+++ lib/freesamples/number1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
 
 class MadeApp extends StatefulWidget {
   const MadeApp({super.key});
@@ -8,7 +9,11 @@ class MadeApp extends StatefulWidget {
 }
 
 class _TextfieldState extends State<MadeApp> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   bool passwordVisible = false;
+  String name = '';
+  String password = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +38,14 @@ class _TextfieldState extends State<MadeApp> {
           ),
         ],
         backgroundColor: Colors.black,
-        title: const Text("SampleApp"),
+        title: const Text(
+          "WELCOME",
+          style: TextStyle(
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
         foregroundColor: Colors.white,
         centerTitle: true,
       ),
@@ -41,14 +53,18 @@ class _TextfieldState extends State<MadeApp> {
         alignment: Alignment.center,
         margin: const EdgeInsets.all(50),
         child: Column(children: [
-          const Text(
-            "HIGH TABLE",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.normal,
-                backgroundColor: Colors.pink),
+          GradientText(
+            'HIGH TABLE',
+            style: const TextStyle(
+              fontSize: 40.0,
+              fontWeight: FontWeight.bold,
+              backgroundColor: Colors.black,
+            ),
+            colors: const [
+              Colors.pink,
+              Colors.red,
+              Colors.teal,
+            ],
           ),
           const SizedBox(
             height: 18,
@@ -59,13 +75,14 @@ class _TextfieldState extends State<MadeApp> {
               child: Column(
                 children: [
                   TextFormField(
+                    controller: _nameController,
                     cursorColor: Colors.green,
                     keyboardType: TextInputType.name,
                     decoration: InputDecoration(
                         contentPadding: const EdgeInsets.all(10),
                         prefixIcon: const Icon(Icons.person),
-                        label: const Text("input username"),
-                        hintText: "username here",
+                        label: const Text("username"),
+                        hintText: "input username",
                         focusedBorder: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(8)),
                         enabledBorder: OutlineInputBorder(
@@ -75,6 +92,7 @@ class _TextfieldState extends State<MadeApp> {
                     height: 30,
                   ),
                   TextFormField(
+                    controller: _passwordController,
                     obscureText: passwordVisible,
                     cursorColor: Colors.green,
                     keyboardType: TextInputType.visiblePassword,
@@ -92,13 +110,38 @@ class _TextfieldState extends State<MadeApp> {
                                 : Icons.visibility_off)),
                         contentPadding: const EdgeInsets.all(10),
                         prefixIcon: const Icon(Icons.person),
-                        label: const Text("input password"),
-                        hintText: "password here",
+                        label: const Text("password"),
+                        hintText: "input password",
+                        hintStyle: const TextStyle(fontSize: 4),
                         focusedBorder: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(8)),
                         enabledBorder: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(8))),
                   ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          name = _nameController.text;
+                          password = _passwordController.text;
+                        });
+                      },
+                      style: const ButtonStyle(
+                        foregroundColor: MaterialStatePropertyAll(Colors.white),
+                        backgroundColor: MaterialStatePropertyAll(Colors.black),
+                      ),
+                      child: const Text("login")),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                        "Name : $name                 Password : $password ",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        )),
+                  )
                 ],
               ),
             ),
