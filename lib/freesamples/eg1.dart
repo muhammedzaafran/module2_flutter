1ed97c6c717f805f9d661556335636e849602e1f
muhammedzaafran <zaafranmuhammed@gmal.com>
2023-12-19 10:56:31 +0530

    Done.

--- lib/freesamples/eg1.dart
+++ lib/freesamples/eg1.dart
@@ -8,10 +8,12 @@ class FormFieldSample extends StatefulWidget {
 }
 
 class _FormFieldSampleState extends State<FormFieldSample> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool passwordVisible = false;
+  final _loginForm = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    final TextEditingController _nameController = TextEditingController();
-    final TextEditingController _passwordController = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.teal,
@@ -32,44 +34,84 @@ class _FormFieldSampleState extends State<FormFieldSample> {
                 ),
               ),
               Form(
+                  key: _loginForm,
                   child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      cursorColor: Colors.green,
-                      keyboardType: TextInputType.emailAddress,
-                      controller: _nameController,
-                      decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.all(10),
-                          prefixIcon: const Icon(Icons.person),
-                          label: const Text("Email"),
-                          hintText: "input email",
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8)),
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8))),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      cursorColor: Colors.green,
-                      keyboardType: TextInputType.visiblePassword,
-                      controller: _passwordController,
-                      decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.all(10),
-                          prefixIcon: const Icon(Icons.password),
-                          label: const Text("Password"),
-                          hintText: "input password",
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8)),
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8))),
-                    ),
-                  )
-                ],
-              ))
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          cursorColor: Colors.green,
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Please enter your email";
+                            } else if (!value.contains("@gmail")) {
+                              return "please enter a valid email";
+                            } else {
+                              return null;
+                            }
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                          controller: _nameController,
+                          decoration: InputDecoration(
+                              contentPadding: const EdgeInsets.all(10),
+                              prefixIcon: const Icon(Icons.person),
+                              label: const Text("Email"),
+                              hintText: "input email",
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8)),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8))),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          validator: (value) {
+                            RegExp regex = RegExp(
+                                r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$&*~]).{8,}$');
+                            if (value!.isEmpty) {
+                              return "Please enter your password";
+                            } else {
+                              if (!regex.hasMatch(value)) {
+                                return 'please enter a valid password';
+                              } else {
+                                return null;
+                              }
+                            }
+                          },
+                          obscureText: passwordVisible,
+                          cursorColor: Colors.green,
+                          keyboardType: TextInputType.visiblePassword,
+                          controller: _passwordController,
+                          decoration: InputDecoration(
+                              contentPadding: const EdgeInsets.all(10),
+                              prefixIcon: const Icon(Icons.password),
+                              suffixIcon: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      passwordVisible = !passwordVisible;
+                                    });
+                                  },
+                                  icon: Icon(passwordVisible
+                                      ? Icons.visibility
+                                      : Icons.visibility_off)),
+                              label: const Text("Password"),
+                              hintText: "input password",
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8)),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8))),
+                        ),
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            if (_loginForm.currentState!.validate()) {
+
+                            }
+                          },
+                          child: const Text("Submit"))
+                    ],
+                  ))
             ]),
       ),
     );
