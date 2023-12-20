239a6cb68aa74f678285ba296bb4a693fd63de81
muhammedzaafran <zaafranmuhammed@gmal.com>
2023-12-20 10:45:05 +0530

    Done.

--- lib/freesamples/loginform.dart
+++ lib/freesamples/loginform.dart
@@ -12,17 +12,46 @@ class _LoginFormState extends State<LoginForm> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("S A M P L E   L O G I N   F O R M",
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 15,
-          color: Colors.black,
-        ),
+        title: const Text(
+          "S A M P L E   L O G I N   F O R M",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+            color: Colors.black,
+          ),
         ),
         centerTitle: true,
         backgroundColor: Colors.white,
       ),
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.white,
+      body: Container(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.all(58),
+        child: const Column(
+          children: [
+            Text(
+              "L O G I N",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Form(
+              child: SizedBox(
+                child: Column(
+                  children: [
+
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
